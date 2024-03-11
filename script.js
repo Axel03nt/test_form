5230f05c4da1a5f738ed9b64b92a00257a6f0c62
Axel Tolentino <57690314+Axel03nt@users.noreply.github.com>
2024-03-11 11:47:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showform() {
+    var boton = document.getElementById("show-form");
+    var formularioContainer = document.getElementById("form_container");
+    boton.style.display = "none";
+    formularioContainer.style.display = "block";
+  }
+  
